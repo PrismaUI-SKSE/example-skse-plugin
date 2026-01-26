b6e3b95195367cb3b0c2b8729e22af24a51da21a
StarkMP <stark.maks.pacs@gmail.com>
2026-01-26 15:42:15 +0300

    chore: update for 1.2.0

--- xmake.lua
+++ xmake.lua
@@ -4,7 +4,7 @@ set_xmakever("2.8.2")
 includes("lib/commonlibsse-ng")
 
 set_project("PrismaUI-Example-Plugin")
-set_version("1.1.0")
+set_version("1.2.0")
 set_license("GPL-3.0")
 
 set_languages("c++23")
